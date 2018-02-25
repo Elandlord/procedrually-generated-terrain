1018e4c0f236d0f09b82a79ca4a5cab424a3d9dc
Eric Landheer <e.landheer@mentechmedia.nl>
2018-02-25 13:48:09 +0100

    :zap: Added FPS Controller, Day + Night system, altered Perlin Noise randomness

--- Assets/Scripts/Blocks.cs
+++ Assets/Scripts/Blocks.cs
@@ -12,7 +12,10 @@ public class Blocks : MonoBehaviour
 	//This divides the noise frequency
 	public float NoiseSize = 10.0f;
 
-	public float bottomY = 30f;
+	public float bottomY = 15f;
+
+	private bool generated;
+	private float randomFloat;
 
 	private GameObject root;
 
@@ -20,33 +23,41 @@ public class Blocks : MonoBehaviour
 	public Vector2 Size;
 	public float terrainSize;
 
-	public Camera character;
+	// FPS Controller
+	public GameObject character;
+
+	// Main camera
+	public GameObject mainCamera;
+
+	public Material grass;
+	public Material dirt;
+	public Material water;
+
+	void Awake() {
+		randomFloat = Random.Range (0.0f, 5.0f);
+	}
 
 	void OnGUI ()
 	{
+		if (!generated) {
+			terrainSize = Mathf.RoundToInt(GUI.HorizontalSlider(new Rect(10, 50, 100, 30), terrainSize, 0.0F, 100.0F));
 
-		terrainSize = Mathf.RoundToInt(GUI.HorizontalSlider(new Rect(10, 50, 100, 30), terrainSize, 0.0F, 100.0F));
+			GUI.Label(new Rect(10, 10, 250, 50), "Size:" + " X:" + terrainSize + " Y:" + terrainSize);
+			Size = new Vector2 (terrainSize, terrainSize);
 
-		GUI.Label(new Rect(10, 10, 250, 50), "Size:" + " X:" + terrainSize + " Y:" + terrainSize);
-
-		Size = new Vector2 (terrainSize, terrainSize);
-
-		//Make a button that generates when you press it
-		if(GUI.Button( new Rect( 10, 100, 100, 30 ), "Generate" ))
-		{
-
-			//Generate!
-			Generate();
-
+			//Make a button that generates when you press it
+			if(GUI.Button( new Rect( 10, 100, 100, 30 ), "Generate" ))
+			{
+				Generate();
+			}
 		}
-
 	}
 
 	//Function that inputs the position and spits out a float value based on the perlin noise
 	public float PerlinNoise(float x, float y)
 	{
 		//Generate a value from the given position, position is divided to make the noise more frequent.
-		float noise = Mathf.PerlinNoise( x / NoiseSize, y / NoiseSize );
+		float noise = Mathf.PerlinNoise( (x + randomFloat) / NoiseSize, y / NoiseSize );
 
 		//Return the noise value
 		return noise * Height;
@@ -61,7 +72,7 @@ public class Blocks : MonoBehaviour
 	//Call this function to generate the terrain
 	void Generate ()
 	{
-
+		generated = true;
 		//If we find a gameobject called terrain, there's a high
 		//chance that we have the previous terrain still there
 		//So, let's delete it
@@ -73,7 +84,13 @@ public class Blocks : MonoBehaviour
 
 		//Put the root object at the center of the boxes
 		root.transform.position = new Vector3( Size.x/2, 0, Size.y/2 );
-		character.transform.position = new Vector3 (Size.x / 2, 10, Size.y / 2);
+
+		// Set character to Active to get it to spawn
+		character.SetActive (true);
+
+		// Set MainCamera to false to prevent it to interfere with the camera of the character
+		mainCamera.SetActive (false);
+
 
 		//For loop for x-axis
 		for(int x = 0; x <= Size.x; x++)
@@ -86,11 +103,16 @@ public class Blocks : MonoBehaviour
 
 				for(int depth = 0; depth <= bottomY; depth++)
 				{
-					if (depth > 20) {
-						GenerateCube (x, noiseY - 20, z);
-					} else {
-						GenerateCube (x, noiseY - depth, z);
+					if (depth == 0) {
+						if (Mathf.Round(noiseY) == 2) {
+							GenerateCube (x, noiseY, z, water);
+						} else {
+							GenerateCube (x, noiseY , z, grass);
+						}
+					} else if (depth <= 10){
+						GenerateCube (x, noiseY - depth, z, dirt);
 					}
+
 				}
 			}
 
@@ -101,10 +123,17 @@ public class Blocks : MonoBehaviour
 
 	}
 
-	void GenerateCube(float x, float y, float z) 
+	void GenerateCube(float x, float y, float z, Material material = default(Material)) 
 	{
 		GameObject box = GameObject.CreatePrimitive(PrimitiveType.Cube);
-		box.GetComponent<Renderer>().material.color = GenerateRandomColor();
+
+		// if material is not set, use default
+		if(material != null) {
+			box.GetComponent<Renderer> ().material = material;
+		}else {
+			box.GetComponent<Renderer>().material.color = GenerateRandomColor();
+		}
+
 		box.transform.position = new Vector3( Mathf.Round(x), Mathf.Round(y) , Mathf.Round(z));
 		box.transform.parent = root.transform;
 	}
---
+++ Assets/Scripts/DayNight.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DayNight : MonoBehaviour {
+
+	public float CycleMins;
+	public float CycleCalc;
+	public float OneCycle;
+	public float time;
+	public int startDay = 0;
+
+	public GameObject displayDay;
+
+	public string[] weekdays = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+	// Use this for initialization
+	void Start () {
+		displayDay.GetComponent<Text> ().text = weekdays [startDay];
+		CycleMins = 5f;
+		OneCycle = 60 * CycleMins;
+		CycleCalc = 0.1f / CycleMins * -1f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if(Time.timeScale != 0){
+			time += Time.deltaTime;
+			transform.Rotate (0, 0, CycleCalc, Space.World);
+		}
+
+		if(time >= OneCycle) {
+			time = time - OneCycle;
+			startDay++;
+			displayDay.GetComponent<Text>().text = weekdays[startDay%weekdays.Length];
+		}
+
+	}
+
+}
